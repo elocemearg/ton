54468ddb910056f60998dbef8e08d9affefe24d3
Graeme Cole <graeme@greem.co.uk>
2022-09-22 20:49:34 +0100

    Change default IPv4 multicast group
    The IPv4 multicast address ought to be in the 239.192.0.0/14 range.

--- src/defaults.h
+++ src/defaults.h
@@ -2,10 +2,14 @@
 #define _DEFAULTS_H
 
 /* Arbitrary multicast address which the announcer multicasts to and the
- * listener subscribes to. */
-#define TON_MULTICAST_GROUP_IPV4 "239.14.42.200"
+ * listener subscribes to. This is in the 239.192.0.0/14 range for multicast
+ * on private networks. */
+#define TON_MULTICAST_GROUP_IPV4 "239.199.77.5"
 
-/* Randomly chosen ff08:... address */
+/* Randomly chosen ff08:... address for announcements on IPv6 multicast.
+ * ff08:... is the range for private multicast addresses with
+ * organization-local scope. I generated a random 112-bit group ID and
+ * called it mine. */
 #define TON_MULTICAST_GROUP_IPV6 "ff08:5206:dd93:4290:0891:d264:1444:bd21"
 
 /* Default port number on which to send UDP datagrams announcing ourselves,
