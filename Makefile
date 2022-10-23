1a99e3707305621e91f5ff0f41a7b56e07605b36
Graeme Cole <graeme@greem.co.uk>
2022-10-23 15:28:08 +0100

    Add licensing information and version number
    Version number is now 1.0.0.
    
    Add licence.txt, to say that ton is released under the 3-Clause BSD
    Licence.
    
    Add some more ton subcommands to display the licence and third-party
    notices if necessary. If libssl and libcrypto are statically compiled
    into the binary, "ton notices" shows notices required by the OpenSSL
    licence.

--- Makefile
+++ Makefile
@@ -35,19 +35,32 @@ TON_H_FILES:=$(wildcard src/*.h)
 GIT_COMMIT_HASH:=$(shell git rev-parse HEAD || echo unknown)
 
 # Linux executable: build with TON_C_FILES and the word list file.
-ton: $(TON_C_FILES) $(TON_H_FILES) src/wordlist.txt
-	$(CC) $(CFLAGS) -Wpedantic -DUNIX=1 -DTON_GIT_COMMIT_HASH="\"$(GIT_COMMIT_HASH)\"" -o ton $(TON_C_FILES) -Wl,--format=binary -Wl,src/wordlist.txt -Wl,--format=default $(LDFLAGS)
+ton: $(TON_C_FILES) $(TON_H_FILES) src/wordlist.txt licence.txt
+	$(CC) $(CFLAGS) -Wpedantic -DUNIX=1 \
+		-DTON_GIT_COMMIT_HASH="\"$(GIT_COMMIT_HASH)\"" \
+		-o ton \
+		$(TON_C_FILES) \
+		-Wl,--format=binary -Wl,src/wordlist.txt -Wl,licence.txt -Wl,--format=default \
+		$(LDFLAGS)
 
 # Windows executable: check that MINGW_OPENSSL_ROOT and MINGW_CUNIT_ROOT are
 # set (MINGW_CUNIT_ROOT will be the dummy "." if we're not actually building
 # the unit tests). If they are, build a static executable for Windows.
-ton.exe: $(TON_C_FILES) $(TON_H_FILES) src/wordlist.txt
+ton.exe: $(TON_C_FILES) $(TON_H_FILES) src/wordlist.txt licence.txt
 ifndef MINGW_OPENSSL_ROOT
 	$(error "ton for Windows requires a MinGW-compiled OpenSSL library. You need to build OpenSSL with MinGW, then set MINGW_OPENSSL_ROOT to the directory containing libcrypto.a and libssl.a, then rerun this make.")
 else ifndef MINGW_CUNIT_ROOT
 	$(error "You need to build CUnit with MinGW, then set MINGW_CUNIT_ROOT to the top-level directory containing install/lib/libcunit.a, then rerun this make.")
 else
-	$(MINGW_CC) $(CFLAGS) -DWINDOWS=1 -DTON_GIT_COMMIT_HASH="\"$(GIT_COMMIT_HASH)\"" -static $(MINGW_OPENSSL_INCLUDE) $(MINGW_CUNIT_INCLUDE) -o ton.exe $(TON_C_FILES) -Wl,--format=binary -Wl,src/wordlist.txt -Wl,--format=default -L$(MINGW_OPENSSL_ROOT) $(MINGW_LINK_CUNIT_PATH) $(LDFLAGS) -liphlpapi -lws2_32
+	$(MINGW_CC) $(CFLAGS) -DWINDOWS=1 -DTON_CONTAINS_OPENSSL=1 \
+		-DTON_GIT_COMMIT_HASH="\"$(GIT_COMMIT_HASH)\"" \
+		-static \
+		$(MINGW_OPENSSL_INCLUDE) $(MINGW_CUNIT_INCLUDE) \
+		-o ton.exe \
+		$(TON_C_FILES) \
+		-Wl,--format=binary -Wl,src/wordlist.txt -Wl,licence.txt -Wl,--format=default \
+		-L$(MINGW_OPENSSL_ROOT) $(MINGW_LINK_CUNIT_PATH) \
+		$(LDFLAGS) -liphlpapi -lws2_32
 endif
 
 # Man pages need to be gzipped before being copied to $(INSTALL_MAN_DEST_DIR)
--- src/ton.c
+++ src/ton.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <errno.h>
 
 #include "tonpush.h"
 #include "tonpull.h"
@@ -13,11 +14,71 @@
 #define TON_GIT_COMMIT_HASH "unknown"
 #endif
 
+#define TON_VERSION_STRING "1.0.0"
+
+/* Resource: licence.txt */
+extern const char _binary_licence_txt_start[];
+extern const char _binary_licence_txt_end[];
+
+void
+print_main_help(FILE *out) {
+    fprintf(out,
+"Main commands:\n"
+"    ton push [options] <file> ...    # send files\n"
+"    ton pull [options]               # receive files\n"
+"Run ton push -h or ton pull -h for help.\n"
+"\n"
+"Other commands:\n"
+"    ton help                         # show this help\n"
+#ifdef TON_UNIT_TESTS
+"    ton test                         # run unit tests\n"
+#endif
+"    ton version                      # show version information\n"
+"    ton licence                      # show licensing information\n"
+#ifdef TON_CONTAINS_OPENSSL
+"    ton notices                      # show notices about third-party software\n"
+#endif
+    );
+}
+
 void
 print_version(void) {
-    printf("ton\n");
+    printf("ton %s\n", TON_VERSION_STRING);
+    printf("Copyright 2022 by Graeme Cole <graeme@greem.co.uk>\n");
+    printf("Released under the 3-Clause BSD License (see \"ton licence\" for details).\n");
+#ifdef TON_CONTAINS_OPENSSL
+    printf("This binary has parts of OpenSSL compiled into it (see \"ton notices\").\n");
+#endif
     printf("git commit hash: %s\n", TON_GIT_COMMIT_HASH);
     printf("This binary was compiled on %s at %s\n", __DATE__, __TIME__);
+}
+
+void
+print_licence(void) {
+    /* Print the contents of licence.txt */
+    size_t length = _binary_licence_txt_end - _binary_licence_txt_start;
+    size_t bytes_written = 0;
+
+    while (bytes_written < length) {
+        size_t ret = fwrite(_binary_licence_txt_start + bytes_written, 1, length - bytes_written, stdout);
+        if (ret == 0) {
+            fprintf(stderr, "ton: %s\n", strerror(errno));
+        }
+        bytes_written += ret;
+    }
+}
+
+void
+print_notices(void) {
+#ifdef TON_CONTAINS_OPENSSL
+    /* If this binary was statically built then it contains OpenSSL code in
+     * binary form, so we should include the required notices in accordance
+     * with the OpenSSL licence. */
+    printf("This binary has parts of OpenSSL statically compiled into it.\n");
+    printf("The OpenSSL license can be found here:\nhttps://www.openssl.org/source/license-openssl-ssleay.txt\n");
+    printf("This product includes cryptographic software written by Eric Young (eay@cryptsoft.com).\n");
+    printf("This product includes software written by Tim Hudson (tjh@cryptsoft.com).\n");
+#endif
 }
 
 int main(int argc, char **argv) {
@@ -35,12 +96,7 @@ int main(int argc, char **argv) {
     ton_sockets_setup();
 
     if (argc < 2) {
-        fprintf(stderr,
-"Usage:\n"
-"    ton push [options] <file> ...\n"
-"    ton pull [options]\n"
-"    ton version\n"
-"Run ton push -h or ton pull -h for further help.\n");
+        print_main_help(stderr);
         exit(1);
     }
 
@@ -57,8 +113,17 @@ int main(int argc, char **argv) {
     else if (!strcmp(verb, "version")) {
         print_version();
     }
+    else if (!strcmp(verb, "license") || !strcmp(verb, "licence")) {
+        print_licence();
+    }
+    else if (!strcmp(verb, "notices")) {
+        print_notices();
+    }
+    else if (!strcmp(verb, "help")) {
+        print_main_help(stdout);
+    }
     else {
-        fprintf(stderr, "Unknown command %s\nTry \"ton push\" or \"ton pull\".\n", verb);
+        fprintf(stderr, "Unknown command \"%s\".\nTry \"ton push\" or \"ton pull\".\n", verb);
         ret = 1;
     }
 
