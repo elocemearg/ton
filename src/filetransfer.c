elocemearg/ton

commit 1c3e1dc9af8366440ba15bc02d5346b21704fb64

Don't follow symlinks for files named on command line
Don't use realpath() to resolve pathnames given to us, because that also
resolves symlinks and we don't want it to do that. If a user tries to
ton push something with a wildcard in it and a symlink is one of the
files matched by the wildcard, we want to send the symlink not the file
to which it points.

This has the possibly unwanted side-effect that if you do "ton push foo"
and foo happens to be a symlink, you now push the symlink and nothing
else. If it becomes a problem we could add an option to say "resolve
symlinks for files named on the command line", but for now it doesn't do
that.

src/filetransfer.c
@@ -69,41 +69,6 @@ ends_with_icase(TON_LF_CHAR *path, TON_LF_CHAR *ending) {
 }
 #endif
 
-static TON_LF_CHAR *
-alloc_real_path_name(const TON_LF_CHAR *path) {
-    TON_LF_CHAR *real_path_name;
-    size_t len;
-
-    real_path_name = ton_realpath(path);
-    if (real_path_name == NULL)
-        return NULL;
-
-    /* Remove any trailing directory separators, because Windows doesn't like
-     * it when you try to stat "c:\foo\bar\" - it prefers "c:\foo\bar".
-     *
-     * On Linux, don't remove the first character from the string, so that
-     * if all we have is "/", we keep that.
-     *
-     * On Windows, don't remove a backslash if it immediately follows a colon,
-     * so if we have "D:\", we want to keep it as "D:\" (the root directory
-     * of the D drive) not "D:" (our current directory on the D drive).
-     *
-     * We don't have to deal with the case where we have a drive letter and a
-     * relative path ("D:my\relative\path") because _fullpath() above has
-     * given us an absolute path.
-     */
-    len = ton_lf_len(real_path_name);
-    while (len > 1 && real_path_name[len - 1] == DIR_SEP) {
-#ifdef WINDOWS
-        if (real_path_name[len - 2] == ':')
-            break;
-#endif
-        real_path_name[--len] = '\0';
-    }
-    return real_path_name;
-}
-
-
 static int
 ton_dir_walk_aux(TON_LF_CHAR *path, int orig_path_start,
         int (*callback)(void *cookie, TON_LF_CHAR *file_path,
@@ -243,36 +208,26 @@ ton_dir_walk(TON_LF_CHAR *path,
         int (*callback)(void *cookie, TON_LF_CHAR *file_path,
             TON_STAT *st, int orig_path_start),
         void *cookie) {
-    int rc;
     int pos;
-    TON_LF_CHAR *full_path = alloc_real_path_name(path);
-    if (full_path == NULL) {
-        ton_error(0, errno, TON_LF_PRINTF, path);
-        return -1;
-    }
 
-    /* If full_path is a file, point orig_path_start to the start of the file's
+    /* If path is a file, point orig_path_start to the start of the file's
      * basename.
-     * If full_path is "/", orig_path_start is 1.
-     * If full_path is a directory, point orig_path_start to the start of the
+     * If path is "/", orig_path_start is 1.
+     * If path is a directory, point orig_path_start to the start of the
      * last directory component of the path. */
-    pos = ton_lf_len(full_path);
+    pos = ton_lf_len(path);
     if (pos > 0)
         --pos;
 
     /* Find the last directory separator, and we want to point to the character
      * after it. */
-    while (pos > 0 && full_path[pos] != DIR_SEP)
+    while (pos > 0 && path[pos] != DIR_SEP)
         pos--;
 
-    if (full_path[pos] == DIR_SEP)
+    if (path[pos] == DIR_SEP)
         pos++;
 
-    rc = ton_dir_walk_aux(full_path, pos, callback, cookie);
-
-    free(full_path);
-
-    return rc;
+    return ton_dir_walk_aux(path, pos, callback, cookie);
 }
 
 /*

src/localfs.c
@@ -324,15 +324,6 @@ ton_access(const TON_LF_CHAR *path, int mode) {
 #endif
 }
 
-TON_LF_CHAR *
-ton_realpath(const TON_LF_CHAR *path) {
-#ifdef LOCAL_FILENAME_IS_WCHAR
-    return _wfullpath(NULL, path, 0);
-#else
-    return realpath(path, NULL);
-#endif
-}
-
 const TON_LF_CHAR *
 ton_lf_basename(const TON_LF_CHAR *path) {
     const TON_LF_CHAR *bn = path + ton_lf_len(path);

src/localfs.h
@@ -178,9 +178,6 @@ ton_utime(const TON_LF_CHAR *path, TON_UTIMBUF *timbuf);
 int
 ton_access(const TON_LF_CHAR *path, int mode);
 
-TON_LF_CHAR *
-ton_realpath(const TON_LF_CHAR *path);
-
 /* Return a pointer to the basename part of the given path. */
 const TON_LF_CHAR *
 ton_lf_basename(const TON_LF_CHAR *path);

commit 9442e3807a4445bf0f24b5553fb83c0272bdb079

Resolve . and .. components of ton-pushed paths, but not symlinks
Now I remember why ton push called realpath() on the paths named on the
command line. It's so that if we're in the directory "/foo/bar" and we
run "ton push ." we transfer a directory called "bar" and not a
directory called ".".

This desirable behaviour was broken by commit 1c3e1dc9, "Don't follow
symlinks for files named on command line".

However, we still don't want to follow symlinks in paths given to ton
push. So rather than using realpath(), we now use ton_dedotify_path(),
which canonicalizes the path (makes the path absolute, replaces
repeated directory separators with a single separator, and resolves
"." and ".." components) but doesn't follow symlinks.

Unit test added for ton_dedotify_path().

src/filetransfer.c
@@ -175,6 +175,41 @@ ton_dir_walk_aux(TON_LF_CHAR *path, int orig_path_start,
     return ret;
 }
 
+static TON_LF_CHAR *
+alloc_real_path_name(const TON_LF_CHAR *path) {
+    TON_LF_CHAR *real_path_name;
+    size_t len;
+
+    real_path_name = ton_dedotify_path(path);
+    if (real_path_name == NULL)
+        return NULL;
+
+    /* Remove any trailing directory separators, because Windows doesn't like
+     * it when you try to stat "c:\foo\bar\" - it prefers "c:\foo\bar".
+     *
+     * On Linux, don't remove the first character from the string, so that
+     * if all we have is "/", we keep that.
+     *
+     * On Windows, don't remove a backslash if it immediately follows a colon,
+     * so if we have "D:\", we want to keep it as "D:\" (the root directory
+     * of the D drive) not "D:" (our current directory on the D drive).
+     *
+     * We don't have to deal with the case where we have a drive letter and a
+     * relative path ("D:my\relative\path") because _fullpath() above has
+     * given us an absolute path.
+     */
+    len = ton_lf_len(real_path_name);
+    while (len > 1 && real_path_name[len - 1] == DIR_SEP) {
+#ifdef WINDOWS
+        if (real_path_name[len - 2] == ':')
+            break;
+#endif
+        real_path_name[--len] = '\0';
+    }
+    return real_path_name;
+}
+
+
 /* Recursively search the directory named in "path", calling callback for each
  * file found. If "path" is a file not a directory, we just call callback once
  * for that file.
@@ -209,25 +244,38 @@ ton_dir_walk(TON_LF_CHAR *path,
             TON_STAT *st, int orig_path_start),
         void *cookie) {
     int pos;
+    int rc;
+    TON_LF_CHAR *full_path;
 
-    /* If path is a file, point orig_path_start to the start of the file's
+    /* Do not resolve symlinks but make the path absolute and resolve any
+     * "." or ".." directory components. */
+    full_path = alloc_real_path_name(path);
+    if (full_path == NULL) {
+        ton_error(0, errno, "failed to resolve path: %s", path);
+        return -1;
+    }
+
+    /* If full_path is a file, point orig_path_start to the start of the file's
      * basename.
-     * If path is "/", orig_path_start is 1.
-     * If path is a directory, point orig_path_start to the start of the
+     * If full_path is "/", orig_path_start is 1.
+     * If full_path is a directory, point orig_path_start to the start of the
      * last directory component of the path. */
-    pos = ton_lf_len(path);
+    pos = ton_lf_len(full_path);
     if (pos > 0)
         --pos;
 
     /* Find the last directory separator, and we want to point to the character
      * after it. */
-    while (pos > 0 && path[pos] != DIR_SEP)
+    while (pos > 0 && full_path[pos] != DIR_SEP)
         pos--;
 
-    if (path[pos] == DIR_SEP)
+    if (full_path[pos] == DIR_SEP)
         pos++;
 
-    return ton_dir_walk_aux(path, pos, callback, cookie);
+    rc = ton_dir_walk_aux(full_path, pos, callback, cookie);
+
+    free(full_path);
+    return rc;
 }
 
 /*

src/localfs.c
@@ -337,6 +337,154 @@ ton_lf_basename(const TON_LF_CHAR *path) {
 }
 
 #ifndef WINDOWS
+static TON_LF_CHAR *
+ton_getcwd(void) {
+    size_t cwd_size = 100;
+    char *cwd = malloc(cwd_size);
+    char *ret;
+    do {
+        ret = getcwd(cwd, cwd_size);
+        if (ret == NULL) {
+            if (errno == ERANGE) {
+                char *new_cwd = realloc(cwd, cwd_size * 2);
+                if (new_cwd == NULL) {
+                    free(cwd);
+                    return NULL;
+                }
+                cwd = new_cwd;
+                cwd_size *= 2;
+            }
+            else {
+                free(cwd);
+                return NULL;
+            }
+        }
+    } while (ret == NULL);
+    return cwd;
+}
+#endif
+
+TON_LF_CHAR *
+ton_dedotify_path(const TON_LF_CHAR *path) {
+#ifdef WINDOWS
+    /* On Windows just use _wfullpath() - we don't care about resolving
+     * symlinks because we're going to stick our fingers in our ears and
+     * pretend there aren't any. */
+    return _wfullpath(NULL, path, 0);
+#else
+    TON_LF_CHAR *ret = NULL;
+    int r, w, len, skip_components;
+
+    if (path[0] != DIR_SEP) {
+        /* If the path is not absolute, make it absolute. */
+        TON_LF_CHAR *cwd = ton_getcwd();
+        if (cwd == NULL) {
+            goto nomem;
+        }
+        ret = malloc(ton_lf_len(path) + ton_lf_len(cwd) + 2);
+        if (ret == NULL) {
+            free(cwd);
+            goto nomem;
+        }
+        sprintf(ret, "%s/%s", cwd, path);
+    }
+    else {
+        /* Make a copy of path and put it in ret. */
+        ret = malloc(ton_lf_len(path) + 1);
+        if (ret == NULL) {
+            goto nomem;
+        }
+        strcpy(ret, path);
+    }
+
+    /* We do the following transformations:
+     * Any two or more consecutive DIR_SEP characters get shrunk into one.
+     * Any directory component named "." is removed.
+     * Any directory component named ".." is removed, and we also remove the
+     * previous component. So "/foo/bar/../baz" becomes "/foo/baz".
+     *
+     * Each transformation can only make the string shorter, not longer. */
+
+    /* Rewrite the string in place, but not writing a DIR_SEP character if
+     * that's the last character we wrote. */
+    w = 0;
+    for (r = 0; ret[r]; r++) {
+        if (ret[r] != DIR_SEP || w == 0 || ret[w - 1] != DIR_SEP) {
+            ret[w++] = ret[r];
+        }
+    }
+    ret[w] = '\0';
+
+    /* Remove "." components. Note that we made the path absolute so the
+     * path will never begin with a dot. */
+    w = 0;
+    for (r = 0; ret[r]; r++) {
+        if (ret[r] == DIR_SEP && ret[r + 1] == '.' && (ret[r + 2] == DIR_SEP || ret[r + 2] == '\0')) {
+            /* Skip this directory component. Increment r to point to the dot,
+             * then the for loop increment will point it to the next DIR_SEP,
+             * which we'll copy. */
+            r++;
+        }
+        else {
+            ret[w++] = ret[r];
+        }
+    }
+
+    /* The above can result in the leading slash getting removed, if for
+     * example we have "/." - we want "/" not "". */
+    if (r > 0 && w == 0)
+        ret[w++] = DIR_SEP;
+    ret[w] = '\0';
+
+    /* Remove ".." components. We do this by working backwards from the end.
+     * If we see a ".." component, we put NULs over it and increase the count
+     * of non-.. components before it that we have to skip. */
+    len = strlen(ret);
+    skip_components = 0;
+    w = len;
+    while (w > 0) {
+        /* ret[w] points to the character after a directory component */
+        if (w >= 2 && ret[w - 1] == '.' && ret[w - 2] == '.' &&
+                (w == 2 || ret[w - 3] == DIR_SEP)) {
+            skip_components++;
+            w -= 2;
+            memset(&ret[w], '\0', 3);
+            w--;
+        }
+        else {
+            /* w points to a directory separator or end of string */
+            int comp_start;
+            for (comp_start = w - 1; comp_start >= 0 && ret[comp_start] != DIR_SEP; comp_start--);
+            comp_start++;
+            if (skip_components > 0) {
+                memset(&ret[comp_start], '\0', 1 + w - comp_start);
+                skip_components--;
+            }
+            w = comp_start;
+            if (w > 0)
+                w--;
+        }
+    }
+
+    /* Now remove all '\0' characters from the string, of which we know the
+     * real length. */
+    r = 0;
+    w = 0;
+    for (r = 0; r < len; r++) {
+        if (ret[r] != '\0')
+            ret[w++] = ret[r];
+    }
+    ret[w] = '\0';
+
+    return ret;
+
+nomem:
+    free(ret);
+    return NULL;
+#endif
+}
+
+#ifndef WINDOWS
 char *
 ton_get_symlink_target(TON_LF_CHAR *symlink_path) {
     char *target = NULL;
@@ -616,6 +764,46 @@ ton_lf_from_locale(const char *filename) {
 #include <CUnit/CUnit.h>
 
 static void
+test_ton_dedotify_path(void) {
+#ifndef LOCAL_FILENAME_IS_WCHAR
+    struct {
+        char *path;
+        char *expected;
+    } tests[] = {
+        { "/foo/bar/baz/", "/foo/bar/baz/" },
+        { "/foo/./bar/./baz", "/foo/bar/baz" },
+        { "/foo/././././/////bar///baz", "/foo/bar/baz" },
+        { "/foo/bar/baz/.", "/foo/bar/baz" },
+        { "/.", "/" },
+        { "///.///", "/" },
+        { "/foo/bar/../baz", "/foo/baz" },
+        { "/one/two/../three/four/../five/../..", "/one/" },
+        { "/one/two/../../../", "/" },
+        { "/one/two/three/.././../four/five/six/seven/.././..", "/one/four/five/" },
+        { "/..", "/" },
+        { "/../", "/" },
+        { "/one/../two/../three/../four/five/six", "/four/five/six" },
+        { "/one/two/three/four/five/six/seven/eight/../nine/./ten/../../../../../", "/one/two/three/four/" },
+        { "/one/two/three/four//../five/six/../../..//myfile.txt", "/one/two/myfile.txt" },
+    };
+
+    for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+        char *input_path = tests[i].path;
+        char *expected = tests[i].expected;
+        char *observed;
+
+        observed = ton_dedotify_path(input_path);
+        if (strcmp(observed, expected)) {
+            fprintf(stderr, "test_ton_dedotify_path(): input path \"%s\", expected \"%s\", observed \"%s\"\n",
+                    input_path, expected, observed);
+            CU_FAIL("output not as expected");
+        }
+        free(observed);
+    }
+#endif
+}
+
+static void
 test_ton_lf_from_utf8(void) {
 #ifdef LOCAL_FILENAME_IS_WCHAR
     struct {
@@ -697,6 +885,7 @@ ton_localfs_register_tests(void) {
     CU_TestInfo tests[] = {
         { "ton_lf_from_utf8", test_ton_lf_from_utf8 },
         { "ton_lf_to_utf8", test_ton_lf_to_utf8 },
+        { "ton_dedotify_path", test_ton_dedotify_path },
         CU_TEST_INFO_NULL
     };
 

src/localfs.h
@@ -182,6 +182,12 @@ ton_access(const TON_LF_CHAR *path, int mode);
 const TON_LF_CHAR *
 ton_lf_basename(const TON_LF_CHAR *path);
 
+/* Canonicalize path by shortening any run of more than one directory separator
+ * to a single directory separator, then removing and resolving any "." or ".."
+ * components in the path. However, this does not follow symbolic links. */
+TON_LF_CHAR *
+ton_dedotify_path(const TON_LF_CHAR *path);
+
 #ifndef WINDOWS
 /* Return the target of the given symlink. It is the caller's responsibility to
  * free the return value. On error, NULL is returned. */